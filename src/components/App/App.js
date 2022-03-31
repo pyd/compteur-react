25bbeaf2e16f8047010900fe8df2eccffaacec44
Pierre-Yves DELETTRE <dev.py@delettre.me>
2022-03-31 19:52:25 +0200

    CSS lifting for App component
    use CSS Modules for modularity and avoid
    naming conflicts with child components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
-import Counter from '../OccupancyCounter/OccupancyCounter.js'
+import styles from './App.module.css';
+import OccupancyCounter from '../OccupancyCounter/OccupancyCounter';
 
+//TODO utiliser une classe à la place d'une fonction
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="logo" alt="react logo" />
-        <p className="description">
-          Component : Compteur d'occupation d'une salle
-        </p>
-      </header>
-      <section id="content">
-        <Counter max="12" />
-      </section>
-    </div>
+    <div id={styles.app}> 
+        <header>
+            <img src={logo} className={styles.logo} alt="react logo" />
+            <p className={styles.description}>
+            Component : Compteur d'occupation d'une salle
+            </p>    
+        </header> 
+        <div className={styles.main}>
+            <div className={styles.componentContainer}><OccupancyCounter max="12" /></div>
+        </div>
+    </div>   
   );
 }
 
